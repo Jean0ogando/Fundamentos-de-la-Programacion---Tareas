19af68df21345e7990d6284217d538c2b502c98c
Jean0ogando <134088745+Jean0ogando@users.noreply.github.com>
2023-06-03 23:41:49 -0400

    Add files via upload

---
+++ Tarea3/2.10.c
@@ -0,0 +1,13 @@
+void main(void)
+{
+int NUM;
+printf("Ingrese el numero: ");
+scanf("%d", &NUM);
+if (NUM == 0)
+    printf("\nNulo");
+else
+if (pow (-1, NUM) > 0)
+     printf("\nPar");
+      else
+       printf("\nImpar");
+}
