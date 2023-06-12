4ea985f0b723fb36b7d8c98c9680bde27e32386d
Jean0ogando <134088745+Jean0ogando@users.noreply.github.com>
2023-06-12 13:31:03 -0400

    Add files via upload

---
+++ Tarea 2/1.4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+     float BAS, ALT, SUP;
+     printf("Ingrese la base y la altura del triangulo:");
+    scanf("%f %f", &BAS, &ALT);
+     printf("\n LA superficie del triangulo es: %5.2f", SUP);
+
+}
