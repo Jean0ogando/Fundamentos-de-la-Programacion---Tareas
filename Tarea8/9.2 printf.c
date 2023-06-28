451cd70f1dbc485ba6ba8753ac7f75703f947d03
Jean0ogando <134088745+Jean0ogando@users.noreply.github.com>
2023-06-28 16:58:27 -0400

    Add files via upload

---
+++ Tarea8/9.2 printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(void)
+{
+   char p1;
+   FILE *ar;
+   if((ar = fopen("arc.txt", "r")) != NULL)
+   {
+       while(!feof(ar))
+       {
+           p1 = fgetc(ar);
+
+           printf("%c", p1);
+       }
+       fclose(ar);
+   }
+   else
+    printf("No se puede abir el archivo");
+}
